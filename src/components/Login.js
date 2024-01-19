d28ecc268b4317b9287ee54ae2a115607d2b770d
Sathvik Andhavarapu <sandhav1@terpmail.umd.edu>
2024-01-19 15:46:25 +0530

    created post request for login page - does not work, need to learn abt csrf tokens/jwt tokens

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,37 @@ const Login = () => {
     const [password, setPassword] = useState("");
     
     const handleLogin = () => {
-        
+        // URL where you want to make the POST request
+        const url = 'http://127.0.0.1:8000/account/login/'; 
+
+        const data = {
+          username: username,
+          password: password
+        }
+        // Options for the fetch request
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json', // Set the content type based on your data format
+
+            // You can also include other headers if needed, like authorization headers
+          },
+          body: JSON.stringify(data) // Convert data to JSON string for the request body
+        };
+
+        // Make the POST request using fetch
+        fetch(url, options)
+          .then(response => response.json()) // Parse the response as JSON
+          .then(data => {
+            console.log('Registration request successful:', data);
+            // Handle the response data as needed
+            
+          })
+          
+          .catch(error => {
+            console.error('Error making registration request:', error);
+            // Handle errors
+          });
         
     };
     
